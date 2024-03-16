a9067d5fb9c59c50b9a9f31dec24bfb0c6011132
Hermawan18 <hermawan18.hty@gmail.com>
2024-03-16 12:12:01 +0700

    nambah screen detail

--- Donor-link/components/CardChat.js
+++ Donor-link/components/CardChat.js
@@ -1,9 +1,17 @@
 import { Image, Pressable, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 export default function CardChat({ data }) {
+  const navigation = useNavigation();
   return (
     <View style={{ backgroundColor: 'white', marginBottom: 5 }}>
-      <Pressable onPress={() => console.log('Click ke detail')}>
+      <Pressable
+        onPress={() =>
+          navigation.navigate('Detail', {
+            postId: data.id,
+          })
+        }
+      >
         <View style={{ display: 'flex', flexDirection: 'row', padding: 5 }}>
           <Image source={'#'} style={{ backgroundColor: 'black', width: 50, height: 50, borderRadius: 50 }} />
           <Text style={{ marginLeft: 5, fontSize: 18, fontWeight: 'bold', textAlignVertical: 'center' }}>{data.user.name}</Text>
