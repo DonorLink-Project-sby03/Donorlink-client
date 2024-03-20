28312405e4c979b300d5a487ce31fb3eaca09420
Hermawan18 <hermawan18.hty@gmail.com>
2024-03-20 11:36:18 +0700

    feat: maps direction

--- Donor-link/instance/config.js
+++ Donor-link/instance/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const serverUrl = 'https://dafa-182-253-50-43.ngrok-free.app';
+const serverUrl = 'https://89a2-103-165-209-194.ngrok-free.app';
 const instance = axios.create({ baseURL: serverUrl });
 
 export default instance;
